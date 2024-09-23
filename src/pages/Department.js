791f44d5ec8d209a7c9954349958dca835b5281a
tayyab2914 <tayyab1234rehman@gmail.com>
2024-09-24 01:34:01 +0500

    Modify nothing

--- src/pages/Department.js
+++ src/pages/Department.js
@@ -29,7 +29,6 @@ const Department = () => {
     setDepartmentData(response);
     // console.log(response)
   };
-
   useEffect(() => {
     setDepartment(department_name);
     handleDepartmentChange(department_name);
