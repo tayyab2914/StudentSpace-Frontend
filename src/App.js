5636b8841a55de639bcc2174be70e94e1d215c6e
muhaman <muhamanibutt@gmail.com>
2024-10-04 16:01:09 +0500

    added adsense script

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { message } from 'antd'; // Import message from Ant Design
 import ProjectRoutes from './pages/ProjectRoutes';
 import { ensureGtag, trackPageVisit } from './analytics/analytics_invokers';
 import Description from './components/Home/Description';
+import { PRODUCTION } from './values';
 // import Description from './components/Description';
+import { Helmet } from 'react-helmet';
 
 function App() {
   // Initialize message API
@@ -13,10 +15,15 @@ trackPageVisit()
 ensureGtag()
 },[])
   return (
+    
     <div className="App">
       {contextHolder} {/* Make sure contextHolder is included */}
       <ProjectRoutes />
-      
+      {PRODUCTION && (
+        <Helmet>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1691781600316534" crossorigin="anonymous" ></script>
+        </Helmet>
+      )}
       <Description /> 
     </div>
   );
